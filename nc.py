396b7e8a42d5be35c4a920f2e53f7dcfe6a3b489
Demur Rumed <gunkmute@gmail.com>
2012-08-17 16:11:21 +0000

    How I learned to stop worrying and love ninja

---
+++ nc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+def main():
+	from subprocess import call
+	from tempfile import NamedTemporaryFile
+	conf=open("conf.ninja")
+	build=open("build.ninja","w")
+	fl=conf.readline()
+	if fl[:2]=="#!":s=fl[2:].split()
+	else:
+		s=["python"]
+		build.write(fl)
+	for l in conf:
+		if l=="%\n":
+			x=[]
+			while 1:
+				l=conf.readline()
+				if l=="%\n":break
+				x+=l,
+			build.flush()
+			i=NamedTemporaryFile("w")
+			i.write("".join(x))
+			i.flush()
+			call(s+[i.name],stdout=build)
+		else:build.write(l)
+	build.write("\n")
+main()
